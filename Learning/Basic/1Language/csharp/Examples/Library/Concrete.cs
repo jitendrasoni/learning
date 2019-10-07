fd0c93dbc544175b9a9824d748dbdc22fcebcc62
Jitendra Soni <jitusonijk@gmail.com>
2019-10-07 15:09:54 +1100

    update

---
+++ Learning/Basic/1Language/csharp/Examples/Library/Concrete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+   // If fully completed all functionality and ready to 
+    //provide service is called concrete class Real Time example :A complete Building
+
+    public Class1()
+	{
+	}
+}
