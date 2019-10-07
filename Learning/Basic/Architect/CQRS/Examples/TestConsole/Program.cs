49a4fbb27a2939f95a7dc3bbba085ba4f8410a21
Jitendra Soni <jitusonijk@gmail.com>
2019-10-08 07:15:34 +1100

    test project changes

--- Learning/Basic/Architect/CQRS/Examples/TestConsole/Program.cs
+++ Learning/Basic/Architect/CQRS/Examples/TestConsole/Program.cs
@@ -1,23 +1,63 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProductDatabaseInMemory objnew = new ProductDatabaseInMemory();
+            foreach (var item in objnew.GetItems())
+            {
+                Console.WriteLine("  Direct from product DB " +  item.Id + "  - " + item.Name);
+                
+            }
+
+            //DTO
+
+            Console.ReadLine();
         }
     }
 
+    class command
+    {
+        public void Execute<t>()
+        {
+            //var data = t();
+        }
+    }
 
+    class productDTO
+    {
+        public int Id { get; set; }
+    }
 
     /// <summary>
     /// Define a simple product class here with two fields
     /// </summary>
     class product
     {
-        public string Id { get; set;}
+        public int Id { get; set;}
         public string Name { get; set;}
+    }
+    class ProductDatabaseInMemory
+    {
+       readonly List<product> ProductData = new List<product>();
+       public List<product> GetItems ()
+        {
+            ProductData.Add(new product
+            {
+                Id = 1,
+                Name = "a"
+            });
+
+            ProductData.Add(new product
+            {
+                Id = 2,
+                Name = "b"
+            });
+
+            return ProductData;
+        }
     }
 }
