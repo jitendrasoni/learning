a59f261cce789a0a0220d3ccd735af1ab6624d4b
Jitendra Soni <jitusonijk@gmail.com>
2019-10-12 16:26:37 +1100

    Update the strcuture for the DP

---
+++ Learning/Basic/2DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Design Patterns.
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+
+    class DesignPatternsFactoy <t> where t : IdesignPatternExample
+    {
+        private readonly IdesignPatternExample _IdesignPatternExample;
+        public DesignPatternsFactoy(t DesignPatternExample)
+        {
+            _IdesignPatternExample = DesignPatternExample;
+        }
+    }
+
+    interface IdesignPatternExample
+    {
+        public void Execute();
+    }
+    class Singleton : IdesignPatternExample
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
